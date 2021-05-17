9e71ea978a9fb97a2bb40c3c25c882304bfdb549
habib <habibdev96@gmail.com>
2021-05-16 23:56:57 -0600

    created accordion functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// accordion
+const accordion = () => {
+  const faqLink = document.querySelectorAll(".faq__link");
+
+  faqLink.forEach((link) => {
+    link.addEventListener("click", () => {
+      link.parentElement.classList.toggle("active");
+    });
+  });
+};
+accordion();
